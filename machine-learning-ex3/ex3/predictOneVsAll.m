6cde6158ede85628ba53342e9e495813848d14f1
YhgzXxfz <qingfeiyou@gmail.com>
2016-10-23 19:10:24 +0200

    feat(ex3 one-vs-all): implement prediction

--- machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -29,11 +29,8 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-
-
-
-
-
+grades = sigmoid(X*all_theta');
+[max_grade, p] = max(grades, [], 2);
 
 
 % =========================================================================
