3628a1efec9af235faf37b210c5f0dcbfaac5ae7
YhgzXxfz <qingfeiyou@gmail.com>
2016-10-24 21:51:28 +0200

    feat(ex5): implement regularized gradient

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -21,7 +21,7 @@ grad = zeros(size(theta));
 h = X*theta;
 theta(1, :) = zeros(1, size(theta,2));
 J = 1/(2*m) * sum((h-y).^2) + lambda/(2*m) * sum(theta.^2);
-
+grad = 1/m * (X'*(h-y)) + lambda/m * theta;
 % =========================================================================
 
 grad = grad(:);
